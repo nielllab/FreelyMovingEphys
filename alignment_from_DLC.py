4652dfd3ce26e7db80de95f1e1250bdcf75acaae
dylanmmartins <dylanmichaelmartins@gmail.com>
2020-06-01 15:30:54 -0700

    progress towards aligning head, funcitons for interpolating and dropping NaNs

--- alignment_from_DLC.py
+++ alignment_from_DLC.py
@@ -6,7 +6,7 @@ Reads in .csv file from deepLabCut, computes head position.
 
 Adapted from /niell-lab-analysis/freely moving/alignHead.m
 
-last modified: May 29, 2020
+last modified: June 1, 2020
 """
 #####################################################################################
 import pandas as pd
@@ -14,63 +14,141 @@ import numpy as np
 from matplotlib import pyplot as plt
 import xarray as xr
 
-def align_head_from_DLC(topdown_data, num_points=8, thresh=0.98, figures=False): #default num_points set to 8; 9&10 would include cricket points in Angie's prey capture data
+####################################################
+def drop_leading_and_lagging_nans(data, loc_names):
+    for loc_num in range(0, len(loc_names)):
+        this_loc_name = loc_names[loc_num]
+        loc_data = data.sel(point_loc=this_loc_name)
+        # find first and last non-NaN value and drop everything that comes before and after these
+        # ends up with an xarray with real start and end points instead of filled NaN values
+        true_where_valid = pd.notna(loc_data)
+        index_of_valid = [i for i, x in enumerate(true_where_valid) if x]
+        if index_of_valid != []:
+            # index into valid positions and select valid data
+            first_valid = index_of_valid[0]
+            last_valid = index_of_valid[-1]
+            valid_data = data.sel(frame=range(first_valid, last_valid))
+        elif index_of_valid == []:
+            print('no NaNs could be found')
+    return valid_data
 
-    # IMPLEMENT: for statement to index through each trial individually
+####################################################
+def interp_nans_if_any(whole_data, loc_names):
+    '''
+    Runs NaN interpolation of xarray DataArray
+    :param whole_data: DataArray of positions
+    :param coords_array: array of coord names of whole_data
+    :return: interpolated DataArray of same coords and dims
+    '''
+    for loc_num in range(0, len(loc_names)):
+        # get the name of the current point location, index into whole_data to get the data that will be interpolated
+        # then, delete unneeded coords of data_to_interp
+        this_loc_name = loc_names[loc_num]
+        data_to_interp = whole_data.sel(point_loc=this_loc_name)
+        del data_to_interp['point_loc'], data_to_interp['trial']
 
-    # get all the point location names
-    coords_array = topdown_data.coords['point_loc']
-    del coords_array['point_loc'] # deletes the coordinates but retains the array of coord names
+        # do the linear interpolation
+        interp_item = xr.DataArray.interpolate_na(data_to_interp, dim='frame', use_coordinate='frame', method='linear')
 
-    # for each point location in the topdown input data, select y head points and subtract them from 1200 to fix coordinates
-    for pt_loc in range(0,len(coords_array)):
-        pt_str = coords_array[pt_loc]
-        if ' y' in pt_str:
-            topdown_data.pt_str = topdown_data.pt_str - 1200
+        # rebuild the post-interpolation DataArray
+        if loc_num == 0:
+            interp_data = xr.DataArray(interp_item)
+            interp_data['point_loc'] = this_loc_name
+        elif loc_num > 0:
+            interp_item_to_concat = xr.DataArray(interp_item)
+            interp_item_to_concat['point_loc'] = this_loc_name
+            interp_data = xr.concat([interp_data, interp_item_to_concat], dim='point_loc', fill_value='NaN')
+    return interp_data
 
-    # figure of nose position over time, with start and finish labeled in green and red respectively
-    if figures==True:
-        nose_x_pts = topdown_data.sel(point_loc='nose x')
-        nose_y_pts = topdown_data.sel(point_loc='nose y')
-        plt.figure()
-        plt.plot(np.squeeze(nose_x_pts), np.squeeze(nose_y_pts))
-        plt.plot((np.squeeze(nose_x_pts)[0]), (np.squeeze(nose_y_pts)[0]), 'go') # starting point
-        plt.plot((np.squeeze(nose_x_pts)[-1]), (np.squeeze(nose_y_pts)[-1]), 'ro')  # ending point
-        plt.show()
+####################################################
+def align_head_from_DLC(all_topdown_data, trial_list, pt_names, coord_correction_val=1200, num_points=8, thresh=0.99, figures=False):
+    '''
+    Aligns the head of a mouse from DLC output files which are passed in from load_all_csv.py
 
-    # threshold points using the input paramater, thresh, to find all times when all points are good (only want high values)
-    good_topdown_data = topdown_data
-    num_good_topdown_pts = topdown_data
-    for pt_loc in range(0, len(coords_array)):
-        current_pt_loc = coords_array[pt_loc]
-        pt_str = current_pt_loc
-        if 'likelihood' in pt_str:
-            good_topdown_data.pt_str = topdown_data.where(topdown_data.pt_str >= thresh, topdown_data.pt_str < thresh)
-            num_good_topdown_pts.pt_str = good_topdown_data.where(good_topdown_data.pt_str, good_topdown_data.pt_str < thresh)
+    :param all_topdown_data: one xarray DataArray containing the coordinates of all points for all trials
+    :param trial_list: list of unique strings identifying each trial in all_topdown_data
+    :param pt_names: list of names of point locations on mouse
+    :param coord_correction_val: value to subtract from y-axis coordinates
+    :param num_points: number of points recorded from DLC
+    :param thresh: likelihood threshold value
+    :param figures: bool, determines whether or not figures will be printed out for each trial
+    :return:
 
-    # this block isn't working quite yet
-    print(num_good_topdown_pts)
-    only_good_pts = num_good_topdown_pts == num_points
-    bad_frac = 1 - (num_good_topdown_pts / num_points)
-    print(bad_frac)
+    other notes
+    default num_points set to 8; 9&10 would include cricket points in Angie's prey capture data
+    '''
 
-    # NOTE: This figure doesn't work quite yet either
-    if figures==True:
-        plt.subplots(1,2)
-        plt.subplot(121)
-        plt.plot(num_good_topdown_pts)
-        plt.title('number of good timepoints by frame')
-        plt.ylabel('# good points')
-        plt.xlabel('frame #')
-        plt.ylim([0, num_points])
-        plt.subplot(122)
-        plt.bar(x=range(0, num_points), height=bad_frac)
-        plt.title('fraction of timepoints below threshold')
-        plt.ylabel('fraction of bad timepoints')
-        plt.xlabel('point #')
-        plt.show()
+    # run through each trial individually
+    for trial_num in range(0, len(trial_list)):
+        # get the name of the current trial
+        current_trial = trial_list[trial_num]
 
-    return good_topdown_data
+        with all_topdown_data.sel(trial=current_trial) as topdown_data:
+
+            # get all the point location names
+            coords_array = topdown_data.coords['point_loc']
+            del coords_array['point_loc'] # deletes the coordinates but retains the array of coord names
+
+            # for each point location in the topdown input data, select y head points and subtract them from 1200 to fix coordinates
+            for pt_loc in range(0, len(coords_array)):
+                pt_str = coords_array[pt_loc]
+                if ' y' in pt_str:
+                    topdown_data.pt_str = topdown_data.pt_str - coord_correction_val
+
+            # interpolate across NaNs fro each point_loc, then piece dataset back together
+            topdown_interp = interp_nans_if_any(topdown_data, pt_names)
+
+            # THOUGHT: eliminate NaNs at the start and end of each trial and point so that plots show true start and finish
+            # topdown_interp_leadlagdrop = drop_leading_and_lagging_nans(topdown_interp, pt_names)
+
+            # make figure of nose position over time, with start and finish labeled in green and red respectively
+            if figures==True:
+                # for now, just drop NaNs that remain in the topdown_interp xarray after interpolation
+                nose_x_pts = topdown_interp.sel(point_loc='nose x', drop='NaN')
+                nose_y_pts = topdown_interp.sel(point_loc='nose y', drop='NaN')
+                plt.figure()
+                plt.plot(np.squeeze(nose_x_pts), np.squeeze(nose_y_pts))
+                plt.plot((np.squeeze(nose_x_pts)[0]), (np.squeeze(nose_y_pts)[0]), 'go') # starting point
+                plt.plot((np.squeeze(nose_x_pts)[-1]), (np.squeeze(nose_y_pts)[-1]), 'ro')  # ending point
+                plt.show()
+
+            # threshold points using the input paramater, thresh, to find all times when all points are good (only want high values)
+            only_good_topdown_data = topdown_data
+            sum_of_bad_topdown_data = topdown_data
+            for pt_loc in range(0, len(coords_array)):
+                current_pt_loc = coords_array[pt_loc]
+                pt_str = current_pt_loc
+                if 'likelihood' in pt_str:
+                    # keep first argument, turn second argument to NaN, and drop second argument in bad_topdown_data_dropped
+                    only_good_topdown_data.pt_str = topdown_data.where(topdown_data.pt_str >= thresh, topdown_data.pt_str < thresh)
+                    sum_of_bad_topdown_data.pt_str = pd.nansum(only_good_topdown_data)
+                    print('only good')
+                    print(only_good_topdown_data)
+                    print('sum of bad')
+                    print(sum_of_bad_topdown_data)
+
+    # # this block isn't working quite yet
+    # only_good_pts = num_good_topdown_pts == num_points
+    # bad_frac = 1 - (num_good_topdown_pts / num_points)
+    # print(bad_frac)
+    #
+    # # NOTE: This figure doesn't work quite yet either
+    # if figures==True:
+    #     plt.subplots(1,2)
+    #     plt.subplot(121)
+    #     plt.plot(num_good_topdown_pts)
+    #     plt.title('number of good timepoints by frame')
+    #     plt.ylabel('# good points')
+    #     plt.xlabel('frame #')
+    #     plt.ylim([0, num_points])
+    #     plt.subplot(122)
+    #     plt.bar(x=range(0, num_points), height=bad_frac)
+    #     plt.title('fraction of timepoints below threshold')
+    #     plt.ylabel('fraction of bad timepoints')
+    #     plt.xlabel('point #')
+    #     plt.show()
+
+    return topdown_data
 
 ###############################################################
     # good_points = np.where(raw_data > thresh)
--- load_all_csv.py
+++ load_all_csv.py
@@ -7,7 +7,7 @@ Loads in top-down camera and right or left eye from DLC outputs and data are ali
 Requires alignment_from_DLC.py
 Adapted from /niell-lab-analysis/freely moving/loadAllCsv.m
 
-last modified: May 29, 2020
+last modified: June 1, 2020
 """
 #####################################################################################
 from glob import glob
@@ -27,13 +27,15 @@ def read_dlc(dlcfile):
     pts = pd.read_hdf(dlcfile)
     # organize columns of pts
     pts.columns = [' '.join(col[:][1:3]).strip() for col in pts.columns.values]
-    return pts
+    pt_loc_names = pts.columns.values
+    return pts, pt_loc_names
 
 ####################################
-def read_in_eye(data_input, side):
+def read_in_eye(data_input, side, num_points=8):
     # create list of eye points that matches data variables in data xarray
     eye_pts = []
-    for eye_pt in range(1, 9):
+    num_points_for_range = num_points + 1
+    for eye_pt in range(1, num_points_for_range):
         eye_pts.append('p' + str(eye_pt) + ' x')
         eye_pts.append('p' + str(eye_pt) + ' y')
         eye_pts.append('p' + str(eye_pt) + ' likelihood')
@@ -47,7 +49,7 @@ def read_in_eye(data_input, side):
     if data_input != None:
         try:
             # read in .h5 file
-            eye_data = read_dlc(data_input)
+            eye_data, eye_names = read_dlc(data_input)
             # turn old and new labels into dictionary so that eye points can be renamed
             eye_data.rename(columns={eye_pts[i]: new_eye_pts[i] for i in range(len(new_eye_pts))})
         # if the trial's main data file wasn't provided, raise error
@@ -58,22 +60,22 @@ def read_in_eye(data_input, side):
         print('no ' + str(side) + ' eye data given')
         eye_data = None
 
-    return eye_data
+    return eye_data, eye_names
 
 ####################################
 def read_data(topdown_input=None, lefteye_input=None, righteye_input=None):
 
     # read top-down camera data into xarray
     if topdown_input != None:
-        topdown_pts = read_dlc(topdown_input)
+        topdown_pts, topdown_names = read_dlc(topdown_input)
     elif topdown_input == None:
         print('no top-down data given')
 
     # read in left and right eye (okay if not provided)
-    lefteye_pts = read_in_eye(lefteye_input, 'left')
-    righteye_pts = read_in_eye(righteye_input, 'right')
+    lefteye_pts, lefteye_names = read_in_eye(lefteye_input, 'left')
+    righteye_pts, righteye_names = read_in_eye(righteye_input, 'right')
 
-    return topdown_pts, lefteye_pts, righteye_pts
+    return topdown_pts, topdown_names, lefteye_pts, lefteye_names, righteye_pts, righteye_pts
 
 ####################################
 # find list of all data
@@ -88,37 +90,44 @@ lefteye_file_list = glob(main_path + '*eye2l*DeepCut*.h5')
 # loop through each topdown file and find the associated files
 # then, read the data in for each set, and build from it an xarray DataArray
 loop_count = 0
-limit_of_loops = 1 # for testing purposes, limit number of topdown files read in
+limit_of_loops = 4 # for testing purposes, limit number of topdown files read in
+
+trial_id_list = []
+
 for file in topdown_file_list:
     if loop_count < limit_of_loops:
         split_path = os.path.split(file)
         file_name = split_path[1]
         mouse_key = file_name[0:5]
         trial_key = file_name[17:27]
+
+        # get a the other recorded files that are associated with the topdown file currently being read
         acc_file = ', '.join([i for i in acc_file_list if mouse_key and trial_key in i])
         time_file = ', '.join([i for i in time_file_list if mouse_key and trial_key in i])
         righteye_file = ', '.join([i for i in righteye_file_list if mouse_key and trial_key in i])
         lefteye_file = ', '.join([i for i in lefteye_file_list if mouse_key and trial_key in i])
 
-        topdown_pts, lefteye_pts, righteye_pts = read_data(file, righteye_file, lefteye_file)
+        # run
+        topdown_pts, topdown_names, lefteye_pts, lefteye_names, righteye_pts, righteye_pts = read_data(file, righteye_file, lefteye_file)
 
+        # make a unique name for the mouse and the recording trial
         trial_id = 'mouse_' + str(mouse_key) + '_trial_' + str(trial_key)
-        loop_label = 'trial_' + str(loop_count)
+        trial_id_list.append(trial_id)
 
-        # PROBLEM
-        # intention here is to build one DataArray that stacks up all trials
-        # currently, it seems to be appending the frames, which is not what we want
+        # build one DataArray that stacks up all trials in separate dimensions
         if loop_count == 0:
             topdown = xr.DataArray(topdown_pts)
             topdown = xr.DataArray.rename(topdown, new_name_or_name_dict={'dim_0': 'frame', 'dim_1': 'point_loc'})
+            topdown['trial'] = trial_id
         elif loop_count > 0:
             topdown_trial = xr.DataArray(topdown_pts)
             topdown_trial = xr.DataArray.rename(topdown_trial, new_name_or_name_dict={'dim_0': 'frame', 'dim_1': 'point_loc'})
-            topdown = xr.concat([topdown, topdown_trial], dim='frame', fill_value='NaN')
+            topdown_trial['trial'] = trial_id
+            topdown = xr.concat([topdown, topdown_trial], dim='trial', fill_value='NaN')
 
         # TO DO: align by time files instead of by frames
 
-        # align topdown head points
-        good_aligned_topdown = align_head_from_DLC(topdown, figures=True)
-
         loop_count = loop_count + 1
+
+# align topdown head points
+topdown_aligned = align_head_from_DLC(topdown, trial_id_list, topdown_names, figures=True)
