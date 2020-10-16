0b28bf7f8e94c5e525fdea82901db2c9b9d99d91
dylanmmartins <dylanmichaelmartins@gmail.com>
2020-10-16 14:50:05 -0700

    cleanup

--- dlc/pickle_to_h5.py
+++ dlc/pickle_to_h5.py
@@ -6,12 +6,17 @@ convert any DeepLabcut .pickle outputs to .h5 files
 Sept. 29, 2020
 """
 
-# package imports
 import deeplabcut
 import argparse
 
-# module imports
-from util.read_data import find
+# glob for subdirectories
+def find(pattern, path):
+    result = [] # initialize the list as empty
+    for root, dirs, files in os.walk(path): # walk though the path directory, and files
+        for name in files:  # walk to the file in the directory
+            if fnmatch.fnmatch(name,pattern):  # if the file matches the filetype append to list
+                result.append(os.path.join(root,name))
+    return result # return full list of file of a given type
 
 parser = argparse.ArgumentParser(description='deinterlace videos and adjust timestamps to match')
 parser.add_argument('-c', '--dlc_config',help='DeepLabCut config .yaml path', default='/home/seuss/Desktop/MathisNetwork2/config.yaml')
