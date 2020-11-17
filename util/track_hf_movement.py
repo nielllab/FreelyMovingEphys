9659e05398faa6b3da9e7bfa4bcc48bbb3c81581
kraken <niell.lab.orders@gmail.com>
2020-11-17 11:36:01 -0800

    update ball tracking

--- util/track_hf_movement.py
+++ util/track_hf_movement.py
@@ -16,13 +16,15 @@ from util.read_data import open_time1
 # track the movement of the ball
 # TO DO: add converstion to cm
 def ball_tracking(csv_path, config):
+    screen_center = config['optical_mouse_screen_center']
+    centX = screen_center['x']; centY = screen_center['y']
     # read in one csv file with timestamps, x position, and y position in three columns
     csv_data = pd.read_csv(csv_path)
     # should have date in timestamp
     # output will be in seconds since the midnight before the trial
     time = open_time1(csv_data['Timestamp'])
     # get the speed at each sample time
-    diff = pd.DataFrame([csv_data['Value.X'].diff(),csv_data['Value.Y'].diff()]).T
+    diff = pd.DataFrame([csv_data['Value.X']-centX,csv_data['Value.Y']-centY]).T
     speed = [np.sqrt((diff.iloc[i,0])**2 + (diff.iloc[i,1])**2) for i in range(0,len(csv_data))] # pixels/sample
     # assemble together components
     all_data = pd.DataFrame([time, csv_data['Value.X'], csv_data['Value.Y'], speed]).T
