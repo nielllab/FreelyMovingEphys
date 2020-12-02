top angle in rad, fixed pupil rotation plots

util/track_topdown.py
@@ -40,9 +40,8 @@ def body_angle(pt_input, config, trial_name, top_view):
         y2 = step_pts.sel(point_loc='spine2_y')
         x_dist = x1 - x2
         y_dist = y1 - y2
-        th = np.arctan(ydist/x_dist)
-        th_deg = np.rad2deg(th)
-        angs.append(float(th_deg))
+        th = np.arctan2(ydist/x_dist)
+        angs.append(float(th))
     body_ang = xr.DataArray(angs, dims=['frame'])
 
     return body_ang
@@ -58,9 +57,8 @@ def head_angle1(pt_input, config, trial_name, top_view):
         y2 = step_pts.sel(point_loc='leftear_y')
         x_dist = x1 - x2
         y_dist = y1 - y2
-        th = np.arctan(ydist/x_dist)
-        th_deg = np.rad2deg(th)
-        angs.append(float(th_deg))
+        th = np.arctan2(ydist/x_dist)
+        angs.append(float(th))
     head_theta = xr.DataArray(angs, dims=['frame'])
 
     return head_theta

util/track_world.py
@@ -305,25 +305,15 @@ def find_pupil_rotation(eyevidpath, eyetimepath, trial_name, eyeext, eye_ell_par
         if np.min(rfit_conv_xr[frame,:]) < -10 or np.max(rfit_conv_xr[frame,:]) > 10:
             rfit_conv_xr[frame,:] = np.nan
 
-    # plot rfit for all trials and highlight mean
-    if config['save_figs'] is True:
-        plt.figure()
-        plt.plot(rfit_conv_xr.T, alpha=0.3)
-        plt.plot(np.mean(rfit_conv_xr.T, 1), 'b--')
-        plt.title('convolved rfit for all frames, mean in blue')
-        # plt.ylim([-3,3])
-        pdf.savefig()
-        plt.close()
-
     # correlation across timepoints
-    timepoint_corr_rfit = pd.DataFrame(rfit_conv_xr.values).T.corr()
+    timepoint_corr_rfit = pd.DataFrame(rfit_conv_xr.isel(frame=range(0,3600)).values).T.corr()
 
     # plot the correlation matrix of rfit over all timepoints
     if config['save_figs'] is True:
         plt.figure()
         fig, ax = plt.subplots()
         im = ax.imshow(timepoint_corr_rfit)
-        ax.set_title('correlation of radius fit across timepoints')
+        ax.set_title('correlation of radius fit during first min. of recording')
         ax.set_xticks(np.arange(len(timepoint_corr_rfit)))
         ax.set_yticks(np.arange(len(timepoint_corr_rfit)))
         ax.set_xticklabels(range(1,len(timepoint_corr_rfit)+1))