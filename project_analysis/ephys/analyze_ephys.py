b7292b99a1829068a69b1ee6431f1a24fec4b944
Dylan Martins <dylanmichaelmartins@gmail.com>
2021-02-16 08:48:22 -0800

    fix ephys analysis ball speed read-in

--- project_analysis/ephys/analyze_ephys.py
+++ project_analysis/ephys/analyze_ephys.py
@@ -129,10 +129,10 @@ def run_ephys_analysis(file_dict):
         speed_data = xr.open_dataset(file_dict['speed'])
         spdVals = speed_data.BALL_data
         try:
-            spd = spdVals.sel(move_params = 'cm_per_sec')
+            spd = spdVals.sel(move_params = 'speed_cmpersec')
             spd_tstamps = spdVals.sel(move_params = 'timestamps')
         except:
-            spd = spdVals.sel(frame = 'cm_per_sec')
+            spd = spdVals.sel(frame = 'speed_cmpersec')
             spd_tstamps = spdVals.sel(frame = 'timestamps')
 
 
