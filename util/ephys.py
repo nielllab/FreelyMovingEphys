cce4f3190d0fde9ae7049ae1bc8466cd345b670b
Angie <niell.lab.orders@gmail.com>
2021-02-19 13:23:17 -0800

    Michael

--- util/ephys.py
+++ util/ephys.py
@@ -164,6 +164,11 @@ def ephys_to_dataframe(path,dates,conditions):
         elif any(i in key.split('_')[4] for i in ['fm2','hf5','hf6','hf7','hf8']) and key.split('_')[0] in conditions.get('dates_saline'):
             data['doi'] = 'saline'
 
+        if any(i in key.split('_')[0] for i in conditions.get('dates_predoi')):
+            data['pre/post'] = 'pre'
+        elif any(i in key.split('_')[0] for i in conditions.get('dates_postdoi')):
+            data['pre/post'] = 'post'
+
     all_data = pd.concat([data for key,data in spike_data.items()], keys=[key for key,data in spike_data.items()])
 
     return all_data
