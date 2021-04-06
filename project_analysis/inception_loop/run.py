a52b58b952692ab7d0ae50a9a7ef961d7714d9da
Dylan Martins <dylanmichaelmartins@gmail.com>
2021-04-06 15:24:32 -0700

    inception loop

--- project_analysis/inception_loop/run.py
+++ project_analysis/inception_loop/run.py
@@ -11,11 +11,13 @@ import torch.nn as nn
 import torch
 from torchvision import transforms
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_csv', type=str)
-    parser.add_argument('--test_csv', type=str)
-    parser.add_argument('--root_dir', type=str)
+    parser.add_argument('--train_csv', type=str, default='/home/niell_lab/data/freely_moving_ephys/inception_loop/inputs/WC_Train_Data.csv')
+    parser.add_argument('--test_csv', type=str, default='/home/niell_lab/data/freely_moving_ephys/inception_loop/inputs/WC_Val_Data.csv')
+    parser.add_argument('--root_dir', type=str, default='/home/niell_lab/data/freely_moving_ephys/inception_loop/inputs/')
     args = parser.parse_args()
     return args
 
@@ -26,13 +28,13 @@ def train_loop(dataloader, model, loss_fn, optimizer):
     for batch, X in enumerate(dataloader):
 
         # compute prediction and loss
-        pred = model(X)
+        pred = model(X.to(device))
         # loss = loss_fn(pred, y)
-
+        print(pred.shape) # should be (64, 6, 128, 128)
         # backpropagation
-        optimizer.zero_grad()
+        # optimizer.zero_grad()
         # loss.backward()
-        optimizer.step()
+        # optimizer.step()
 
         # if batch % 100 == 0:
         #     loss, current = loss.item(), batch * len(X)
@@ -58,17 +60,18 @@ def main(train_csv, test_csv, root_dir):
     training_data = WorldcamDataset(train_csv, root_dir, transform=transforms.ToTensor())
     testing_data = WorldcamDataset(test_csv, root_dir, transform=transforms.ToTensor())
 
-    train_dataloader = DataLoader(training_data, batch_size=64)
+    train_dataloader = DataLoader(training_data, batch_size=64) # add num_workers
     test_dataloader = DataLoader(testing_data, batch_size=64)
 
-    input_channels = 15
+    input_channels = 3
     hidden_channels = 2
     input_kern = 7
-    hidden_kern = 32
+    hidden_kern = 27
 
     learning_rate = 1e-3
 
     model = Stacked2dCore(input_channels, hidden_channels, input_kern, hidden_kern)
+    model.to(device)
 
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
@@ -77,7 +80,7 @@ def main(train_csv, test_csv, root_dir):
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         train_loop(train_dataloader, model, loss_fn, optimizer)
-        test_loop(test_dataloader, model, loss_fn)
+        # test_loop(test_dataloader, model, loss_fn)
     print("Done!")
 
 if __name__ == '__main__':
