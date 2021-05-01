c53f0ccd6d09660db804c1ff6debb6a80ddcf680
Dylan Martins <dylanmichaelmartins@gmail.com>
2021-04-30 17:36:26 -0700

    spike history

--- project_analysis/inception_loop/datasets.py
+++ project_analysis/inception_loop/datasets.py
@@ -18,28 +18,28 @@ def pil_loader(path):
         with Image.open(f) as img:
             return img.convert('RGB')
 
-class WorldcamDataset(Dataset):
-    def __init__(self, img_csv, spike_json, root_dir, history_size, transform=None):
-        self.img_data = pd.read_csv(img_csv)
-        with open(spike_json, 'r') as fp:
-            self.spike_data = json.load(fp)
+class WorldcamDataset3D(Dataset):
+    def __init__(self, image_csv, history_size, spike_npy, root_dir, transform=None):
+        self.img_paths = pd.read_csv(image_csv)
         self.root_dir = root_dir
+        self.spike_bins = np.load(spike_npy)
         self.transform = transform
         self.history_size = history_size
 
     def __len__(self):
-        return(len(self.img_data))
+        return(len(self.img_paths))
     
     def __getitem__(self,idx):
-        # images with history
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        sample = []
         for n in range(self.history_size):
-            img_name = self.img_data['N_{:02d}'.format(n)].iloc[idx]
-            spike_arr = self.spike_data
-            img_path = os.path.join(self.root_dir,self.img_data.iloc[idx,1],img_name)
+            img_name = self.img_paths['N_{:02d}'.format(n)].iloc[idx]
+            img_path = os.path.join(self.root_dir,self.img_paths.iloc[idx,1],img_name)
             img = pil_loader(img_path)
             if self.transform:
                 img = self.transform(img)
             sample.append(img)
-        img_sample = torch.cat(sample,dim=0).unsqueeze(0)
-
-        return img_sample, spike_sample
+            
+        sample = torch.cat(sample,dim=0).unsqueeze(0)
+        return sample
