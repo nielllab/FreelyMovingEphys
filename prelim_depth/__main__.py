cb8d6a4c4f15da73d40853e63617754048f5c876
Dylan Martins <dylanmichaelmartins@gmail.com>
2021-07-12 20:17:33 -0700

    better GUIs

--- prelim_depth/__main__.py
+++ prelim_depth/__main__.py
@@ -2,33 +2,39 @@
 __main__.py
 """
 from prelim_depth.prelim_depth import main as run_prelim_depth
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import *
-from tkinter import ttk
+import PySimpleGUI as sg
 
-# select a directory to use for depth
-root = tk.Tk()
-root.withdraw()
-binary_file = filedialog.askopenfilename(title='Select the freely moving ephys binary file:')
-# set up window of probe options
-window = Tk()
-window.title('Select probe mapping')
-window.geometry("500x300+250+100")
-tab_control = ttk.Notebook(window)
-main = ttk.Frame(tab_control)
-tab_control.add(main, text='')
-tab_control.pack(expand=1, fill='both')
-# probe options
-probes = ['default16', 'NN_H16', 'default64', 'NN_H64-LP', 'DB_P64-3', 'DB_P64-8', 'DB_P128-6']
-probe = StringVar()
-probe.set(probes[0])
-probe_menu = OptionMenu(main, probe, *probes)
-probe_menu.grid(column=0, row=0)
-# when 'select' button is clicked, run preliminary depth analysis
-def run_main():
-    run_prelim_depth(binary_file, probe.get())
-    root.destroy()
-run_button = Button(main, text="select", command=run_main)
-run_button.grid(column=1, row=0)
-window.mainloop()
+def make_window(theme):
+    sg.theme(theme)
+    options_layout =  [[sg.Text('Select the model of ephys probe used.')],
+                       [sg.Combo(values=('default16', 'NN_H16', 'default64', 'NN_H64-LP', 'DB_P64-3', 'DB_P64-8', 'DB_P128-6'), default_value='default16', readonly=True, k='-COMBO-', enable_events=True)],
+                       [sg.Text('Select the ephys binary file for a recording.')],
+                       [sg.Button('Open ephys binary file')]]
+    logging_layout = [[sg.Text('Run this module')],
+                      [sg.Button('Run module')],
+                      [sg.Text("Traceback")],
+                      [sg.Output(size=(60,15))]]
+    layout = [[sg.Text('Preliminary estimate of laminar depth', size=(38, 1), justification='center', font=("Times", 16), relief=sg.RELIEF_RIDGE, k='-TEXT HEADING-', enable_events=True)]]
+    layout +=[[sg.TabGroup([[sg.Tab('Options', options_layout),
+               sg.Tab('Run', logging_layout)]], key='-TAB GROUP-')]]     
+    return sg.Window('Preliminary estimate of laminar depth', layout)
+    
+def main():
+    window = make_window(sg.theme())
+    while True:
+        event, values = window.read(timeout=100)
+        if event == 'Open ephys binary file':
+            binary_file = sg.popup_get_file('Choose ephys binary file')
+            print('Binary file: ' + str(binary_file))
+        elif event in (None, 'Exit'):
+            print('Exiting')
+            break
+        elif event == 'Run module':
+            probe = values['-COMBO-']
+            print('Probe: ' + str(probe))
+            run_prelim_depth(binary_file, probe)
+    window.close()
+    exit(0)
+
+if __name__ == '__main__':
+    main()
