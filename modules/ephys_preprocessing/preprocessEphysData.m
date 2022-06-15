2ec57e767c45aac6ecdd176f6bbf25b9e5f6ff36
leviathan <nielllab@users.noreply.github.com>
2022-06-15 09:37:15 -0700

    correct Janus map

--- modules/ephys_preprocessing/preprocessEphysData.m
+++ modules/ephys_preprocessing/preprocessEphysData.m
@@ -4,10 +4,10 @@ medfilt = 1;
 isuint16 = 1;
 % json_path should to .json file with mappings of each probe i.e. {'probe':[1,2,3]}
 json_path = 'C:/Users/Niell Lab/Documents/GitHub/FreelyMovingEphys/config/channel_maps.json';
-% options for probe name are: [default16, NN_H16, default64, NN_H64_LP, DB_P64_3, DB_P64_8, DB_P128_6]
+% options for probe name are: [default16, NN_H16, default64, NN_H64_LP, DB_P64_3, DB_P64_8, DB_P128_6, DB_P128_D]
 % default16 and default64 are ordered sequences (i.e. no remaping done)
 % all remappings should be 1 (not 0) referenced
-probe = 'DB_P128_6';
+probe = 'DB_P128_D';
 
 % get required params for applyCARtoDat
 [chanMap, nchan, subset] = getProbeMap(probe, json_path);
